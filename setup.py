a5ac3b1b0491bc64d8abef1c88d9c92fce1f7ceb
ShimantoRahman <Shimanto.Rahman1@gmail.com>
2024-01-22 12:54:39 +0100

    Add proflogit to build

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ShimantoRahman/empulse",
-    packages=setuptools.find_packages(exclude=['*.proflogit', '*.optimizers']),
+    packages=setuptools.find_packages(),
     install_requires=[
         'numba>=0.57.0',
         'numpy>=1.24.2',
-        'patsy>=0.5.3',
         'scikit_learn>=1.2.1',
         'scipy>=1.10.1',
-        'xgboost>=1.7.4'
+        'xgboost>=1.7.4',
+        'joblib>=1.3.2',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
