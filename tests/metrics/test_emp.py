9a87871d877e1b5bc45355fdb364a502a3e4440a
Shrahman <Shimanto.Rahman@UGent.be>
2024-02-25 11:11:35 +0100

    Add test for mp function

--- tests/metrics/test_emp.py
+++ tests/metrics/test_emp.py
@@ -1,5 +1,5 @@
 import pytest
-from empulse.metrics import emp, empc
+from empulse.metrics import emp, empc, mp, mpc
 
 
 def test_empc_replication():
@@ -33,3 +33,18 @@ def test_empc_replication():
     assert emp_threshold == pytest.approx(empc_threshold, abs=1e-5)
 
 
+def test_mpc_replication():
+    y_true = [0, 1, 0, 1, 0, 1, 0, 1]
+    y_pred = [0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.8, 0.9]
+
+    clv = 200
+    d = 10
+    f = 1
+    gamma = 0.3
+    tp_benefit = clv * (gamma * (1 - (d / clv)) - (f / clv))
+    fp_cost = d + f
+
+    mp_score, mp_threshold = mp(y_true, y_pred, tp_benefit=tp_benefit, fp_cost=fp_cost)
+    mpc_score, mpc_threshold = mpc(y_true, y_pred, clv=clv, incentive_cost=d, contact_cost=f, accept_rate=gamma)
+    assert mp_score == pytest.approx(mpc_score)
+    assert mp_threshold == pytest.approx(mpc_threshold)
