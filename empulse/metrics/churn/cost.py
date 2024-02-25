70a9d507dc64123b1ddb87134837240b5d44f88a
Shrahman <Shimanto.Rahman@UGent.be>
2024-02-25 15:57:05 +0100

    Convert array-like to ndarray

--- empulse/metrics/churn/cost.py
+++ empulse/metrics/churn/cost.py
@@ -100,7 +100,9 @@ def _objective(
         y_true = dtrain.get_label()
     else:
         raise TypeError(f"Expected dtrain to be of type np.ndarray or xgb.DMatrix, got {type(dtrain)} instead.")
-    y_pred = 1 / (1 + np.exp(-y_pred))
+    if isinstance(clv, ArrayLike):
+        clv = np.asarray(clv)
+    y_pred: np.ndarray = 1 / (1 + np.exp(-y_pred))
 
     profits = contact_cost + incentive_cost + y_true * (
             accept_rate * incentive_cost - incentive_cost - clv * accept_rate
