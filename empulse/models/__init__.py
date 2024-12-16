7666454865889d3bca7d0145dc4af427b4c3120d
Shrahman <Shimanto.Rahman@UGent.be>
2024-12-17 00:42:59 +0100

    Add cslogit to models module

--- empulse/models/__init__.py
+++ empulse/models/__init__.py
@@ -2,4 +2,5 @@ from .b2boost import B2BoostClassifier
 from .bias_relabeling import BiasRelabelingClassifier
 from .bias_resampling import BiasResamplingClassifier
 from .bias_reweighing import BiasReweighingClassifier
+from .cslogit import CSLogitClassifier
 from .proflogit import ProfLogitClassifier
