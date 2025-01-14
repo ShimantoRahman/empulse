16631deb491072c639d1829f2bb7c9dcbe5f0dc4
Shrahman <Shimanto.Rahman@UGent.be>
2025-01-14 20:09:22 +0100

    Fix check costs

--- empulse/models/cost_sensitive/_cs_mixin.py
+++ empulse/models/cost_sensitive/_cs_mixin.py
@@ -28,12 +28,23 @@ class CostSensitiveMixin:
         Overwrite costs set in constructor if they are set in the fit/predict method.
         """
 
-        tp_cost, tn_cost, fn_cost, fp_cost = self._set_costs(
-            tp_cost=tp_cost,
-            tn_cost=tn_cost,
-            fn_cost=fn_cost,
-            fp_cost=fp_cost
-        )
+        if tp_cost is Parameter.UNCHANGED:
+            tp_cost = self.tp_cost
+        if tn_cost is Parameter.UNCHANGED:
+            tn_cost = self.tn_cost
+        if fn_cost is Parameter.UNCHANGED:
+            fn_cost = self.fn_cost
+        if fp_cost is Parameter.UNCHANGED:
+            fp_cost = self.fp_cost
+
+        if (all(isinstance(cost, Real) for cost in (tp_cost, tn_cost, fn_cost, fp_cost)) and
+                sum(abs(cost) for cost in (tp_cost, tn_cost, fn_cost, fp_cost)) == 0.0):
+            warnings.warn(
+                "All costs are zero. Setting fp_cost=1 and fn_cost=1. "
+                f"To avoid this warning, set costs explicitly in the {self.__class__.__name__}.{caller}() method.",
+                UserWarning)
+            fp_cost = 1
+            fn_cost = 1
 
         if not isinstance(tp_cost, Real):
             tp_cost = np.asarray(tp_cost)
@@ -43,36 +54,5 @@ class CostSensitiveMixin:
             fn_cost = np.asarray(fn_cost)
         if not isinstance(fp_cost, Real):
             fp_cost = np.asarray(fp_cost)
-
-        all_numbers = all(isinstance(cost, Real) for cost in (tp_cost, tn_cost, fn_cost, fp_cost))
-        all_zeros = all(abs(cost) == 0 for cost in (tp_cost, tn_cost, fn_cost, fp_cost))
-
-        if all_numbers and all_zeros:
-            warnings.warn(
-                "All costs are zero. Setting fp_cost=1 and fn_cost=1. "
-                f"To avoid this warning, set costs explicitly in the {self.__class__.__name__}.{caller}() method.",
-                UserWarning)
-            fp_cost = 1
-            fn_cost = 1
-
-        # return tp_cost
-        return tp_cost, tn_cost, fn_cost, fp_cost
-
-    def _set_costs(
-            self,
-            *,
-            tp_cost: ArrayLike | float | Parameter,
-            tn_cost: ArrayLike | float | Parameter,
-            fn_cost: ArrayLike | float | Parameter,
-            fp_cost: ArrayLike | float | Parameter,
-    ) -> tuple[ArrayLike | float, ArrayLike | float, ArrayLike | float, ArrayLike | float]:
-        if tp_cost is Parameter.UNCHANGED:
-            tp_cost = self.tp_cost
-        if tn_cost is Parameter.UNCHANGED:
-            tn_cost = self.tn_cost
-        if fn_cost is Parameter.UNCHANGED:
-            fn_cost = self.fn_cost
-        if fp_cost is Parameter.UNCHANGED:
-            fp_cost = self.fp_cost
 
         return tp_cost, tn_cost, fn_cost, fp_cost
