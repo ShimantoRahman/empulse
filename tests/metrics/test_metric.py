f5770765ea5efca6321ec7e8ebd0f1dd8318a011
Shrahman <Shimanto.Rahman@UGent.be>
2025-01-22 16:28:20 +0100

    Add tests for Metric class

--- tests/metrics/test_metric.py
+++ tests/metrics/test_metric.py
@@ -4,7 +4,7 @@ from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sympy.stats import Beta
 
-from empulse.metrics import Metric, empc_score
+from empulse.metrics import Metric, empc_score, expected_cost_loss_churn, expected_savings_score, mpc_score
 
 
 @pytest.fixture(scope='module')
@@ -24,20 +24,19 @@ def test_metric_vs_empc_score(
     customer_lifetime_value, incentive_cost, contact_cost, gamma_alpha, gamma_beta, y_true_and_prediction
 ):
     y, y_proba = y_true_and_prediction
-
-    # Define the profit function using the Metric class
     clv, d, f, alpha, beta = sympy.symbols('clv d f alpha beta')
     gamma = Beta('gamma', alpha, beta)
     profit_func = (
-        Metric().add_tp_benefit(gamma * (clv - d - f)).add_tp_benefit((1 - gamma) * -f).add_fp_cost('d + f').build()
+        Metric('max profit')
+        .add_tp_benefit(gamma * (clv - d - f))
+        .add_tp_benefit((1 - gamma) * -f)
+        .add_fp_cost('d + f')
+        .build()
     )
 
-    # Calculate the metric using the Metric class
     metric_result = profit_func(
         y, y_proba, clv=customer_lifetime_value, d=incentive_cost, f=contact_cost, beta=gamma_beta, alpha=gamma_alpha
     )
-
-    # Calculate the metric using the empc_score function
     empc_result = empc_score(
         y,
         y_proba,
@@ -47,6 +46,154 @@ def test_metric_vs_empc_score(
         beta=gamma_beta,
         alpha=gamma_alpha,
     )
-
-    # Assert that the results are the same
     assert pytest.approx(metric_result) == empc_result
+
+
+@pytest.mark.parametrize(
+    'customer_lifetime_value, incentive_cost, contact_cost, accept_rate',
+    [(100, 10, 1, 0.3), (200, 20, 2, 0.2), (150, 15, 1.5, 0.4)],
+)
+def test_metric_vs_mpc_score(customer_lifetime_value, incentive_cost, contact_cost, accept_rate, y_true_and_prediction):
+    y, y_proba = y_true_and_prediction
+    clv, d, f, gamma = sympy.symbols('clv d f gamma')
+    profit_func = (
+        Metric('max profit')
+        .add_tp_benefit(gamma * (clv - d - f))
+        .add_tp_benefit((1 - gamma) * -f)
+        .add_fp_cost('d + f')
+        .build()
+    )
+
+    metric_result = profit_func(
+        y, y_proba, clv=customer_lifetime_value, d=incentive_cost, f=contact_cost, gamma=accept_rate
+    )
+    mpc_result = mpc_score(
+        y,
+        y_proba,
+        clv=customer_lifetime_value,
+        incentive_cost=incentive_cost,
+        contact_cost=contact_cost,
+        accept_rate=accept_rate,
+    )
+    assert pytest.approx(metric_result) == mpc_result
+
+
+@pytest.mark.parametrize(
+    'customer_lifetime_value, incentive_fraction, contact_cost, accept_rate',
+    [(100, 0.05, 1, 0.3), (200, 0.1, 2, 0.2), (150, 0.15, 1.5, 0.4)],
+)
+def test_metric_vs_expected_loss(
+    customer_lifetime_value, incentive_fraction, contact_cost, accept_rate, y_true_and_prediction
+):
+    y, y_proba = y_true_and_prediction
+    clv, delta, f, gamma = sympy.symbols('clv delta f gamma')
+    profit_func = (
+        Metric('cost')
+        .add_tp_benefit(gamma * (clv - delta * clv - f))
+        .add_tp_benefit((1 - gamma) * -f)
+        .add_fp_cost(delta * clv + f)
+        .build()
+    )
+
+    metric_result = profit_func(
+        y, y_proba, clv=customer_lifetime_value, delta=incentive_fraction, f=contact_cost, gamma=accept_rate
+    )
+    cost_result = expected_cost_loss_churn(
+        y,
+        y_proba,
+        clv=customer_lifetime_value,
+        incentive_fraction=incentive_fraction,
+        contact_cost=contact_cost,
+        accept_rate=accept_rate,
+        normalize=True,
+    )
+    assert pytest.approx(metric_result) == cost_result
+
+
+@pytest.mark.parametrize(
+    'customer_lifetime_value, incentive_cost, contact_cost, accept_rate',
+    [(100, 5, 1, 0.3), (200, 10, 2, 0.2), (150, 15, 1.5, 0.4)],
+)
+def test_metric_vs_savings(customer_lifetime_value, incentive_cost, contact_cost, accept_rate, y_true_and_prediction):
+    y, y_proba = y_true_and_prediction
+    clv, d, f, gamma = sympy.symbols('clv d f gamma')
+    profit_func = (
+        Metric('savings')
+        .add_tp_benefit(gamma * (clv - d - f))
+        .add_tp_benefit((1 - gamma) * -f)
+        .add_fp_cost(d + f)
+        .build()
+    )
+
+    metric_result = profit_func(
+        y, y_proba, clv=customer_lifetime_value, d=incentive_cost, f=contact_cost, gamma=accept_rate
+    )
+    tp_cost = (
+        -accept_rate * (customer_lifetime_value - incentive_cost - contact_cost) + (1 - accept_rate) * contact_cost
+    )
+    fp_cost = incentive_cost + contact_cost
+    cost_result = expected_savings_score(
+        y,
+        y_proba,
+        tp_cost=tp_cost,
+        fp_cost=fp_cost,
+    )
+    assert pytest.approx(metric_result) == cost_result
+
+
+def test_alias(y_true_and_prediction):
+    customer_lifetime_value, incentive_fraction, contact_cost, accept_rate = 100, 0.05, 1, 0.3
+    y, y_proba = y_true_and_prediction
+    clv, delta, f, gamma = sympy.symbols('clv delta f gamma')
+    profit_func = (
+        Metric('cost')
+        .add_tp_benefit(gamma * (clv - delta * clv - f))
+        .add_tp_benefit((1 - gamma) * -f)
+        .add_fp_cost(delta * clv + f)
+        .alias({'incentive_fraction': 'delta', 'contact_cost': 'f', 'accept_rate': 'gamma'})
+        .build()
+    )
+    metric_result = profit_func(
+        y,
+        y_proba,
+        clv=customer_lifetime_value,
+        incentive_fraction=incentive_fraction,
+        contact_cost=contact_cost,
+        accept_rate=accept_rate,
+    )
+    cost_result = expected_cost_loss_churn(
+        y,
+        y_proba,
+        clv=customer_lifetime_value,
+        incentive_fraction=incentive_fraction,
+        contact_cost=contact_cost,
+        accept_rate=accept_rate,
+        normalize=True,
+    )
+    assert pytest.approx(metric_result) == cost_result
+
+
+def test_set_default(y_true_and_prediction):
+    customer_lifetime_value, incentive_fraction, contact_cost, accept_rate = 100, 0.05, 1, 0.3
+    y, y_proba = y_true_and_prediction
+    clv, delta, f, gamma = sympy.symbols('clv delta f gamma')
+    profit_func = (
+        Metric('cost')
+        .add_tp_benefit(gamma * (clv - delta * clv - f))
+        .add_tp_benefit((1 - gamma) * -f)
+        .add_fp_cost(delta * clv + f)
+        .alias({'incentive_fraction': 'delta', 'contact_cost': 'f', 'accept_rate': 'gamma'})
+        .set_default(incentive_fraction=0.05, contact_cost=1, accept_rate=0.3, clv=100)
+        .build()
+    )
+    metric_result = profit_func(y, y_proba)
+    cost_result = expected_cost_loss_churn(
+        y,
+        y_proba,
+        clv=customer_lifetime_value,
+        incentive_fraction=incentive_fraction,
+        contact_cost=contact_cost,
+        accept_rate=accept_rate,
+        normalize=True,
+    )
+    assert pytest.approx(metric_result) == cost_result
