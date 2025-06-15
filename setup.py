c859cfff73752a7d501d0b1dd9e0f808815dcd73
ShimantoRahman <shimanto.rahman1@gmail.com>
2025-06-15 17:50:43 +0200

    chore: build package with cython files

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, setup
 
 if __name__ == '__main__':
-    setup()
+    extensions = [
+        Extension('empulse.metrics._loss.loss', ['empulse/metrics/_loss/*.pyx'], include_dirs=[np.get_include()]),
+    ]
+    setup(ext_modules=cythonize(extensions))
