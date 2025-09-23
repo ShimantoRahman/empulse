93cd9aaa7e04860a9e6c9bf6c818e6262d37ff46
ShimantoRahman <Shimanto.Rahman1@gmail.com>
2025-09-23 14:25:41 +0200

    docs(models): add references to other CS models in docs + separate csforest & csbagging in their own file

--- empulse/models/cost_sensitive/__init__.py
+++ empulse/models/cost_sensitive/__init__.py
@@ -1,7 +1,8 @@
 from .b2boost import B2BoostClassifier
 from .cost_threshold import CSThresholdClassifier
+from .csbagging import CSBaggingClassifier
 from .csboost import CSBoostClassifier
-from .csensemble import CSBaggingClassifier, CSForestClassifier
+from .csforest import CSForestClassifier
 from .cslogit import CSLogitClassifier
 from .cstree import CSTreeClassifier
 from .robust_cs import RobustCSClassifier
