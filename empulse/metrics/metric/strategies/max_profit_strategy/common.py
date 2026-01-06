80080f4c61660ddc224a1265c8e635220ee02ea6
ShimantoRahman <shimanto.rahman1@gmail.com>
2026-01-06 09:53:40 +0100

    feat(metrics): allow stochastic variables in cost strategy

---
+++ empulse/metrics/metric/strategies/max_profit_strategy/common.py
@@ -0,0 +1,26 @@
+import sys
+from collections.abc import Iterable
+from typing import Any
+
+import numpy as np
+import sympy
+
+from ....._types import FloatNDArray, IntNDArray
+from ...._cy_convex_hull import convex_hull
+
+if sys.version_info >= (3, 11):
+    pass
+
+
+def _convex_hull(y_true: IntNDArray, y_score: FloatNDArray) -> tuple[IntNDArray, FloatNDArray]:
+    return convex_hull(y_true.astype(np.int32), y_score.astype(np.float64))
+
+
+def extract_distribution_parameters(
+    parameters: dict[str, Any], distribution_args: Iterable[sympy.Symbol]
+) -> tuple[dict[str, Any], dict[str, Any]]:
+    """Extract the distribution parameters from the other parameters."""
+    distribution_parameters = {
+        str(key): parameters.pop(str(key)) for key in distribution_args if str(key) in parameters
+    }
+    return distribution_parameters, parameters
