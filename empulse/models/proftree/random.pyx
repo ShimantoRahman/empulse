971b8976224794841d7a0cb718ccf370d63f5112
ShimantoRahman <shimanto.rahman1@gmail.com>
2026-01-06 09:55:11 +0100

    refactor(models): streamline genetic algorithm operations by removing Children struct and enhancing crossover and mutation functions

--- empulse/models/proftree/random.pyx
+++ empulse/models/proftree/random.pyx
@@ -1,12 +1,12 @@
 from libc.stdlib cimport rand, RAND_MAX
 
-cdef int rand_int(int low, int high) noexcept nogil:
+cdef inline int rand_int(int low, int high) noexcept nogil:
     """Return a random integer in [low, high)."""
     return low + rand() % (high - low)
 
-cdef bint rand_bool() noexcept nogil:
+cdef inline bint rand_bool() noexcept nogil:
     """Return a random boolean value."""
     return rand() % 2 == 0
 
-cdef float rand_fraction() noexcept nogil:
+cdef inline float rand_fraction() noexcept nogil:
     return rand() / (<float>RAND_MAX + 1.0)
