9a0337913ea8207d8170c8b93e0691d639b3a967
ShimantoRahman <shimanto.rahman1@gmail.com>
2026-02-15 23:04:04 +0100

    docs(models): correct typos in docstrings

--- empulse/models/bias_mitigation/bias_relabeling.py
+++ empulse/models/bias_mitigation/bias_relabeling.py
@@ -29,7 +29,7 @@ class BiasRelabelingClassifier(ClassifierMixin, BaseEstimator):  # type: ignore[
         Determines how the group weights are computed.
         Group weights determine how many instances to relabel for each combination of target and sensitive feature.
 
-        - ``'statistical_parity'`` or ``'demographic parity'``: \
+        - ``'statistical parity'`` or ``'demographic parity'``: \
         probability of positive predictions are equal between subgroups of sensitive feature.
 
         - ``Callable``: function which computes the number of labels swaps based on the target and sensitive feature. \
@@ -128,31 +128,6 @@ class BiasRelabelingClassifier(ClassifierMixin, BaseEstimator):  # type: ignore[
             search = GridSearchCV(pipeline, param_grid)
             search.fit(X, y, sensitive_feature=high_clv)
 
-    5. Passing the sensitive feature through metadata routing in a cross-validation grid search:
-
-    .. code-block:: python
-
-        import numpy as np
-        from sklearn import config_context
-        from sklearn.datasets import make_classification
-        from sklearn.linear_model import LogisticRegression
-        from sklearn.model_selection import GridSearchCV
-        from sklearn.pipeline import Pipeline
-        from sklearn.preprocessing import StandardScaler
-        from empulse.models import BiasRelabelingClassifier
-
-        with config_context(enable_metadata_routing=True):
-            X, y = make_classification()
-            high_clv = np.random.randint(0, 2, size=X.shape[0])
-
-            param_grid = {'model__estimator__C': [0.1, 1, 10]}
-            pipeline = Pipeline([
-                ('scaler', StandardScaler()),
-                ('model', BiasRelabelingClassifier(LogisticRegression()).set_fit_request(sensitive_feature=True))
-            ])
-            search = GridSearchCV(pipeline, param_grid)
-            search.fit(X, y, sensitive_feature=high_clv)
-
     References
     ----------
 
@@ -193,7 +168,7 @@ class BiasRelabelingClassifier(ClassifierMixin, BaseEstimator):  # type: ignore[
     @_fit_context(prefer_skip_nested_validation=True)  # type: ignore[misc]
     def fit(self, X: ArrayLike, y: ArrayLike, *, sensitive_feature: ArrayLike | None = None, **fit_params: Any) -> Self:
         """
-        Fit the estimator and reweigh the instances according to the strategy.
+        Fit the estimator and relabels the instances according to the strategy.
 
         Parameters
         ----------
--- empulse/models/bias_mitigation/bias_reweighing.py
+++ empulse/models/bias_mitigation/bias_reweighing.py
@@ -46,7 +46,7 @@ class BiasReweighingClassifier(ClassifierMixin, BaseEstimator):  # type: ignore[
     strategy : {'statistical parity', 'demographic parity'} or Callable, default='statistical parity'
         Determines how the sample weights are computed. Sample weights are passed to the estimator's `fit` method.
 
-        - ``'statistical_parity'`` or ``'demographic parity'``: \
+        - ``'statistical parity'`` or ``'demographic parity'``: \
         probability of positive predictions are equal between subgroups of sensitive feature.
 
         - ``Callable``: function which computes the sample weights based on the target and sensitive feature. \
@@ -117,31 +117,6 @@ class BiasReweighingClassifier(ClassifierMixin, BaseEstimator):  # type: ignore[
         model.fit(X, y, sensitive_feature=high_clv)
 
     4. Passing the sensitive feature in a cross-validation grid search:
-
-    .. code-block:: python
-
-        import numpy as np
-        from sklearn import config_context
-        from sklearn.datasets import make_classification
-        from sklearn.linear_model import LogisticRegression
-        from sklearn.model_selection import GridSearchCV
-        from sklearn.pipeline import Pipeline
-        from sklearn.preprocessing import StandardScaler
-        from empulse.models import BiasReweighingClassifier
-
-        with config_context(enable_metadata_routing=True):
-            X, y = make_classification()
-            high_clv = np.random.randint(0, 2, size=X.shape[0])
-
-            param_grid = {'model__estimator__C': [0.1, 1, 10]}
-            pipeline = Pipeline([
-                ('scaler', StandardScaler()),
-                ('model', BiasReweighingClassifier(LogisticRegression()).set_fit_request(sensitive_feature=True))
-            ])
-            search = GridSearchCV(pipeline, param_grid)
-            search.fit(X, y, sensitive_feature=high_clv)
-
-    5. Passing the sensitive feature through metadata routing in a cross-validation grid search:
 
     .. code-block:: python
 
